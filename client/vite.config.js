8871485bf4bf3a9f9f20f3eb623d26c696fce7d3
totallynotmelol1 <105313252+Veron1234567898@users.noreply.github.com>
2025-05-30 14:12:18 +0400

    s
    s

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,8 +1,14 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { fileURLToPath, URL } from 'url';
 
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  },
   server: {
     port: 3000,
     strictPort: true,
@@ -15,6 +21,7 @@ export default defineConfig({
     outDir: 'dist',
     assetsDir: 'assets',
     emptyOutDir: true,
+    sourcemap: true
   },
   define: {
     'process.env': {}
