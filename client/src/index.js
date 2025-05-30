e9ed5fd634b9de50fc6f8cd3ae91e8552ab117be
totallynotmelol1 <105313252+Veron1234567898@users.noreply.github.com>
2025-05-30 13:58:45 +0400

    s
    s

--- client/src/index.js
+++ client/src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
-import './App.css';
+import './App.css'; // Keep this import if you have global styles
 
 const root = createRoot(document.getElementById('root'));
 root.render(
